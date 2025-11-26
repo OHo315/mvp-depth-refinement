eb25ef092aa8f79a0384205adc2fc6c64b7a5bd3
anthonyceponis <anthonyceponis@gmail.com>
2025-11-26 17:42:30 +0000

    Extent depth anything script to run middlebury and nyu.

--- script/external_models/run-depth-anything.sh
+++ script/external_models/run-depth-anything.sh
@@ -3,16 +3,18 @@
 cd Depth-Anything
 source env/bin/activate
 
+######################
+
 BASE_HYPERSIM_DATASET_DIR=../data/hypersim_processed
 
 for SPLIT in train val test; do
     SPLIT_DIR="$BASE_HYPERSIM_DATASET_DIR/$SPLIT"
+    echo "Running depth anything on hypersim $SPLIT split..."
 
     for dir in "$SPLIT_DIR"/*/; do
         LABELS_DIR="${dir%/}_labels"
         mkdir $LABELS_DIR 
         mv "$dir"depth*  $LABELS_DIR
-        echo "Running depth anything on $SPLIT split..."
         python3 run.py --encoder vits --img-path "$dir" --outdir "$dir" --pred-only
         mv "$LABELS_DIR"/depth* $dir
         rm -rf $LABELS_DIR
@@ -21,4 +23,43 @@ done
 
 echo "Zipping depth_anything processed hypersim..."
 zip -r "$BASE_DATA_DIR/depth_anything_hypersim.zip" "$BASE_HYPERSIM_DATASET_DIR"
+
+######################
+
+BASE_NYU_DATASET_DIR=../data/nyu_v2_processed
+
+for SPLIT in train test; do
+    SPLIT_DIR="$BASE_NYU_DATASET_DIR/$SPLIT"
+    echo "Running depth anything on NYU_V2 $SPLIT split..."
+
+    for dir in "$SPLIT_DIR"/*/; do
+        LABELS_DIR="${dir%/}_labels"
+        mkdir $LABELS_DIR 
+        mv "$dir"depth*  $LABELS_DIR
+        python3 run.py --encoder vits --img-path "$dir" --outdir "$dir" --pred-only
+        mv "$LABELS_DIR"/depth* $dir
+        rm -rf $LABELS_DIR
+    done
+done
+
+echo "Zipping depth_anything processed NYU_V2..."
+zip -r "$BASE_DATA_DIR/depth_anything_nyu_v2.zip" "$BASE_NYU_DATASET_DIR"
+
+
+######################
+
+BASE_MIDDLEBURY_DATASET_DIR=../data/middlebury_processed
+SPLIT_DIR="$BASE_MIDDLEBURY_DATASET_DIR/test"
+LABELS_DIR="${SPLIT_DIR%/}_labels"
+
+mkdir $LABELS_DIR 
+mv "$SPLIT_DIR/"depth*  $LABELS_DIR
+echo "Running depth anything on middlebury $SPLIT split..."
+python3 run.py --encoder vits --img-path "$SPLIT_DIR" --outdir "$SPLIT_DIR" --pred-only
+mv "$LABELS_DIR"/depth* $SPLIT_DIR
+rm -rf $LABELS_DIR
+
+echo "Zipping depth_anything processed middlebury..."
+zip -r "$BASE_DATA_DIR/depth_anything_middlebury.zip" "$BASE_MIDDLEBURY_DATASET_DIR"
+
 
