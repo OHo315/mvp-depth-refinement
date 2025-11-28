4938f04732cc7b7277f0c70bd86ff11e68a01468
Andrew Healey <doolie.healey@gmail.com>
2025-11-28 10:16:29 +0000

    Make infer.py run w/ correct shapes and sizes for diff base MDEstimators

--- ppd_sharpdepth/infer.py
+++ ppd_sharpdepth/infer.py
@@ -33,6 +33,9 @@ import torch.nn.functional as F
 
 import debugpy
 
+from huggingface_hub import hf_hub_download
+from ppd_sharpdepth.ppd.models.ppd import PixelPerfectDepth
+
 if "__main__" == __name__:
     logging.basicConfig(level=logging.INFO)
 
@@ -59,7 +62,7 @@ if "__main__" == __name__:
 
     parser.add_argument("--seed", type=int, default=None, help="Random seed.")
 
-    parser.add_argument("--base_model", type=str, default="unidepth", help="Base model to use for depth estimation. Options: unidepth, depth_anything_small, depth_anything_large")
+    parser.add_argument("--base_model", type=str, default="unidepth", help="Base model to use for depth estimation. Options: unidepth, depth_anything_small, depth_anything_large, pixel_perfect_depth")
     parser.add_argument("--debug", action="store_true", help="Debug mode.")
 
     args = parser.parse_args()
@@ -112,16 +115,19 @@ if "__main__" == __name__:
     if args.base_model == "unidepth":
         unidepth = UniDepthV1.from_pretrained("lpiccinelli/unidepth-v1-vitl14")
         unidepth = unidepth.to(device, dtype=dtype)
-        def base_estimator_fn(marigold_preprocessed_image, raw_image):
-            ret = unidepth.infer((marigold_preprocessed_image*255).squeeze().int())['depth']
-            # print(f"Input to unidepth. shape: {x.shape}, std: {x.std()}, mean: {x.mean()}, dtype: {x.dtype}")
-            # print(f"Output from unidepth. shape: {ret.shape}, std: {ret.std()}, mean: {ret.mean()}, dtype: {ret.dtype}")
+        def base_estimator_fn(marigold_preprocessed_image_1chw, _raw_image_1chw):
+            ret_11hw = unidepth.infer((marigold_preprocessed_image_1chw*255).squeeze().int())['depth']
+            return ret_11hw
+
+            # sanity check, for reference:
+
+            # print(f"Input to unidepth. shape: {marigold_preprocessed_image.shape}, std: {marigold_preprocessed_image.std()}, mean: {marigold_preprocessed_image.mean()}, dtype: {marigold_preprocessed_image.dtype}")
+            # print(f"Output from unidepth. shape: {ret_11hw.shape}, std: {ret_11hw.std()}, mean: {ret_11hw.mean()}, dtype: {ret_11hw.dtype}")
             # raise ValueError("Stop here")
 
             # Input to unidepth. shape: torch.Size([1, 3, 728, 768]), std: 0.32351335883140564, mean: 0.34626907110214233, dtype: torch.float32
             # Output from unidepth. shape: torch.Size([1, 1, 728, 768]), std: 0.360894113779068, mean: 1.302354335784912, dtype: torch.float32
 
-            return ret
     elif args.base_model in ["depth_anything_small", "depth_anything_large"]:
 
         encoder_kind = "vits" if args.base_model == "depth_anything_small" else "vitl"
@@ -142,30 +148,51 @@ if "__main__" == __name__:
             PrepareForNet(),
         ])
 
-        def base_estimator_fn(marigold_preprocessed_image, raw_image):
+        def base_estimator_fn(marigold_preprocessed_image_1chw, raw_image_1chw):
 
-            image = transform({'image': (raw_image/255.).permute(0, 2, 3, 1).squeeze().numpy()})['image']
-            image = torch.from_numpy(image).unsqueeze(0).to(device)
+            image_1hwc = transform({'image': (raw_image_1chw/255.).permute(0, 2, 3, 1).squeeze(0).numpy()})['image'][None]
+            image_1hwc = torch.from_numpy(image_1hwc).to(device)
 
+            depth_raw_1hw = depth_anything(image_1hwc)
 
-            # still sorta mismatches, TODO fix this
-            print(f"Input to depth_anything. shape: {image.shape}, std: {image.std()}, mean: {image.mean()}, dtype: {image.dtype}")
+            depth_resized_11hw = F.interpolate(depth_raw_1hw[None], (marigold_preprocessed_image_1chw.shape[-2], marigold_preprocessed_image_1chw.shape[-1]), mode='bilinear', align_corners=False)
 
-            depth_raw = depth_anything(image)
-            print(f"Output from depth_anything. shape: {depth_raw.shape}, std: {depth_raw.std()}, mean: {depth_raw.mean()}, dtype: {depth_raw.dtype}")
+            return depth_resized_11hw
 
-            depth_resized = F.interpolate(depth_raw[None], (marigold_preprocessed_image.shape[-2], marigold_preprocessed_image.shape[-1]), mode='bilinear', align_corners=False)
+            # sanity check (Good! Matches the stats in submodules/Depth-Anything/run.py):
+            # filename:  submodules/SharpDepth/assets/in-the-wild_example/00.jpg
+            # Input to depth_anything. shape: torch.Size([1, 3, 518, 546]), std: 1.4334324598312378, mean: -0.45399439334869385, dtype: torch.float32
+            # Output from depth_anything. shape: torch.Size([1, 1, 728, 768]), std: 7.16904878616333, mean: 10.526022911071777, dtype: torch.float32
 
-            return depth_resized.unsqueeze(0)
-
-
-            raise NotImplementedError("Depth Anything is not implemented yet")
+            # print(f"Input to depth_anything. shape: {image_1hwc.shape}, std: {image_1hwc.std()}, mean: {image_1hwc.mean()}, dtype: {image_1hwc.dtype}")
+            # print(f"Resized output from depth_anything. shape: {depth_resized_11hw.shape}, std: {depth_resized_11hw.std()}, mean: {depth_resized_11hw.mean()}, dtype: {depth_resized_11hw.dtype}")
+            # raise NotImplementedError("Depth Anything is not implemented yet")
         
-        # sanity check (GOOD! Matches the stats in submodules/Depth-Anything/run.py):
-        # filename:  submodules/SharpDepth/assets/in-the-wild_example/00.jpg
-        # Input to depth_anything. shape: torch.Size([1, 3, 518, 546]), std: 1.4334324598312378, mean: -0.45399439334869385, dtype: torch.float32
-        # Output from depth_anything. shape: torch.Size([1, 518, 546]), std: 7.174661636352539, mean: 10.526067733764648, dtype: torch.float32
-        
+    elif args.base_model == "pixel_perfect_depth":
+
+        ckpt_path = hf_hub_download(repo_id="gangweix/pixel-perfect-depth", filename="ppd.pth")
+        semantics_path = hf_hub_download(repo_id="depth-anything/Depth-Anything-V2-Large", filename="depth_anything_v2_vitl.pth")
+
+        model = PixelPerfectDepth(semantics_pth=semantics_path, sampling_steps=4)
+        model.load_state_dict(torch.load(ckpt_path, map_location='cpu'), strict=False)
+        model = model.to(device).eval()
+
+        def base_estimator_fn(_marigold_preprocessed_image_1chw, raw_image_1chw):
+            H, W = raw_image_1chw.squeeze(0).shape[1:3]
+            raw_image_hwc = raw_image_1chw.squeeze(0).permute(1, 2, 0).numpy()
+            depth_raw_11hw, _ = model.infer_image(raw_image_hwc)
+            depth_11hw = F.interpolate(depth_raw_11hw, size=(H, W), mode='bilinear', align_corners=False)
+            return depth_11hw
+
+            # sanity check (passes!)
+            # Input to pixel perfect depth. shape: (804, 848, 3), std: 82.61926369403086, mean: 88.29934251697487, dtype: uint8
+            # Output from pixel perfect depth. shape: (1, 1, 3, 804), std: 0.3026374280452728, mean: 0.4216078519821167, dtype: float32
+
+            # print(f"Input to pixel perfect depth. shape: {raw_image_hwc.shape}, std: {raw_image_hwc.std()}, mean: {raw_image_hwc.mean()}, dtype: {raw_image_hwc.dtype}")
+            # print(f"Resized output from pixel perfect depth. shape: {depth_11hw.shape}, std: {depth_11hw.std()}, mean: {depth_11hw.mean()}, dtype: {depth_11hw.dtype}")
+            # raise NotImplementedError("Pixel Perfect Depth is not implemented yet")
+
+    
     else:
         raise ValueError(f"Invalid base model: {args.base_model}")
 
@@ -175,7 +202,7 @@ if "__main__" == __name__:
         for batch in tqdm(imgs):
             # Read input image
             rgb = Image.open(os.path.join(input_dir, batch))
-            print("filename: ", os.path.join(input_dir, batch))
+            if args.debug: print("filename: ", os.path.join(input_dir, batch))
             out = pipeline(rgb, base_estimator_fn, processing_res=768, denoising_steps=1)
             depth_colored = out.depth_colored
             unidepth_colored = out.depth_base_colored
