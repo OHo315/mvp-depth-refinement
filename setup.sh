7e2608a4809234fd3e5b78d0b4f66d41e2d0e977
anthonyceponis <anthonyceponis@gmail.com>
2025-11-29 13:58:52 +0000

    Modify setup script to fetch checkpoints for ppd.

--- setup.sh
+++ setup.sh
@@ -37,6 +37,14 @@ source env/bin/activate
 echo "Installing pixel perfect deps"
 pip install -qr requirements.txt
 
+echo "Downloading pixel perfect checkpoint"
+PPD_CHECKPOINT_DIR=submodules/pixel-perfect-depth/checkpoints
+mkdir -p PPD_CHECKPOINT_DIR
+wget -P PPD_CHECKPOINT_DIR "https://huggingface.co/gangweix/Pixel-Perfect-Depth/resolve/main/ppd.pth"
+
+echo "Downloading pixel perfect depth anything v2 dependency checkpoint"
+wget -P PPD_CHECKPOINT_DIR "https://huggingface.co/depth-anything/Depth-Anything-V2-Large/resolve/main/depth_anything_v2_vitl.pth"
+
 popd
 
 pushd submodules/SharpDepth
