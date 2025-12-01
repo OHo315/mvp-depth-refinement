d7a9f099b1b7803253b332a95b5c91a9e90847c6
anthonyceponis <anthonyceponis@gmail.com>
2025-12-01 00:03:59 +0000

    Update diode fetch script.

--- script/data_fetch/diode-indoor.sh
+++ script/data_fetch/diode-indoor.sh
@@ -1,3 +1,4 @@
 echo "Fetching Diode Indoor dataset..."
-mkdir -p $BASE_DATA_DIR
-hf download "bambezius/diode_indoor_train" --repo-type dataset --local-dir $BASE_DATA_DIR
+TARGET_DIR=$BASE_DATA_DIR/diode/indoor/train
+mkdir -p $TARGET_DIR 
+hf download "bambezius/diode_indoor_train" --repo-type dataset --local-dir $TARGET_DIR
