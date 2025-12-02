70bcde7feac3494b4307433cd12503e586f3ae66
anthonyceponis <anthonyceponis@gmail.com>
2025-12-02 20:49:10 +0000

    Add partially implemented depth estimation fetch functions.

---
+++ ppd_sharpdepth/depth_estimators.py
@@ -0,0 +1,144 @@
+from enum import Enum
+import torch
+
+from diffusers import UNet2DConditionModel
+from ppd_sharpdepth.sharpdepth.util.alignment import align_depth_least_square
+from unidepth.models import UniDepthV1
+from .sharpdepth.data.datasets_and_samplers import get_dataset
+from .sharpdepth.data.datasets_and_samplers.base_depth_dataset import (
+    BaseDepthDataset,
+    DatasetMode,
+    DepthFileNameMode,
+    get_pred_name,
+)
+
+from .depth_anything.dpt import DepthAnything
+from .depth_anything.util.transform import Resize, NormalizeImage, PrepareForNet
+from torchvision.transforms import Compose
+import cv2
+
+from huggingface_hub import hf_hub_download
+from ppd_sharpdepth.ppd.models.ppd import PixelPerfectDepth
+
+import torch.nn.functional as F
+
+from typing import Callable
+
+Model = Enum(
+    "Model", 
+    [
+        "sharpdepth_unidepth", 
+        "sharpdepth_depthanything", 
+        "sharpdepth_pixelperfectdepth", 
+        "pixelperfectdepth", 
+        "unidepth", 
+        "lotus"
+    ]
+)
+
+def get_depth_estimator_fn(model: str, device: torch.device, dtype: torch.dtype) -> Callable[[torch.Tensor, torch.Tensor], torch.Tensor]:
+    if base_model == "unidepth":
+        unidepth = UniDepthV1.from_pretrained("lpiccinelli/unidepth-v1-vitl14")
+        unidepth = unidepth.to(device, dtype=dtype)
+        unidepth.requires_grad_(False)
+        def base_depth_estimator_fn(marigold_preprocessed_image_1chw, _raw_image_1chw):
+            ret_11hw = unidepth.infer((marigold_preprocessed_image_1chw*255).squeeze().int())['depth']
+            return ret_11hw
+
+            # sanity check, for reference:
+
+            # print(f"Input to unidepth. shape: {marigold_preprocessed_image.shape}, std: {marigold_preprocessed_image.std()}, mean: {marigold_preprocessed_image.mean()}, dtype: {marigold_preprocessed_image.dtype}")
+            # print(f"Output from unidepth. shape: {ret_11hw.shape}, std: {ret_11hw.std()}, mean: {ret_11hw.mean()}, dtype: {ret_11hw.dtype}")
+            # raise ValueError("Stop here")
+
+            # Input to unidepth. shape: torch.Size([1, 3, 728, 768]), std: 0.32351335883140564, mean: 0.34626907110214233, dtype: torch.float32
+            # Output from unidepth. shape: torch.Size([1, 1, 728, 768]), std: 0.360894113779068, mean: 1.302354335784912, dtype: torch.float32
+
+    elif base_model in ["depth_anything_small", "depth_anything_large"]:
+
+        encoder_kind = "vits" if base_model == "depth_anything_small" else "vitl"
+
+        depth_anything = DepthAnything.from_pretrained('LiheYoung/depth_anything_{}14'.format(encoder_kind)).to(device).eval()
+        depth_anything.requires_grad_(False)
+
+        transform = Compose([
+            Resize(
+                width=518,
+                height=518,
+                resize_target=False,
+                keep_aspect_ratio=True,
+                ensure_multiple_of=14,
+                resize_method='lower_bound',
+                image_interpolation_method=cv2.INTER_CUBIC,
+            ),
+            NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            PrepareForNet(),
+        ])
+
+        def base_depth_estimator_fn(marigold_preprocessed_image_1chw, raw_image_1chw):
+
+            image_1hwc = transform({'image': (raw_image_1chw/255.).permute(0, 2, 3, 1).squeeze(0).float().cpu().numpy()})['image'][None]
+            image_1hwc = torch.from_numpy(image_1hwc).to(device)
+
+            disparity_raw_1hw = depth_anything(image_1hwc)
+            depth_raw_1hw = disparity_raw_1hw.max() - disparity_raw_1hw
+
+            depth_resized_11hw = F.interpolate(depth_raw_1hw[None], (marigold_preprocessed_image_1chw.shape[-2], marigold_preprocessed_image_1chw.shape[-1]), mode='bilinear', align_corners=False)
+
+            return depth_resized_11hw
+
+            # sanity check (Good! Matches the stats in submodules/Depth-Anything/run.py):
+            # filename:  submodules/SharpDepth/assets/in-the-wild_example/00.jpg
+            # Input to depth_anything. shape: torch.Size([1, 3, 518, 546]), std: 1.4334324598312378, mean: -0.45399439334869385, dtype: torch.float32
+            # Output from depth_anything. shape: torch.Size([1, 1, 728, 768]), std: 7.16904878616333, mean: 10.526022911071777, dtype: torch.float32
+
+            # print(f"Input to depth_anything. shape: {image_1hwc.shape}, std: {image_1hwc.std()}, mean: {image_1hwc.mean()}, dtype: {image_1hwc.dtype}")
+            # print(f"Resized output from depth_anything. shape: {disparity_raw_1hw.shape}, std: {disparity_raw_1hw.std()}, mean: {disparity_raw_1hw.mean()}, dtype: {disparity_raw_1hw.dtype}")
+            # raise NotImplementedError("Depth Anything is not implemented yet")
+        
+    elif base_model == "pixel_perfect_depth":
+
+        depth_anything_small_fn = get_base_depth_estimator_fn(base_model="depth_anything_small", device=device, dtype=dtype)
+
+        model = PixelPerfectDepth.from_pretrained("andrew-healey/sharpdepth", subfolder="ppd")
+        model = model.to(device).eval()
+        model.requires_grad_(False)
+
+        def base_depth_estimator_fn(marigold_preprocessed_image_1chw, raw_image_1chw):
+
+            metric_depth_base = depth_anything_small_fn(marigold_preprocessed_image_1chw, raw_image_1chw)
+
+            H, W = marigold_preprocessed_image_1chw.squeeze(0).shape[1:3]
+            raw_image_hwc = raw_image_1chw.squeeze(0).permute(1, 2, 0).float().cpu().numpy()
+            raw_image_hwc_bgr = cv2.cvtColor(raw_image_hwc, cv2.COLOR_RGB2BGR) # infer_image() applies a BGR->RGB conversion, so we must first convert from RGB->BGR here
+            depth_raw_11hw, _ = model.infer_image(raw_image_hwc_bgr)
+            depth_11hw = F.interpolate(depth_raw_11hw, size=(H, W), mode='bilinear', align_corners=False)
+
+            # depth_11hw is in log space
+            # so let's use least-squares to align as closely as possible with log-space metric_depth_base
+            # and then convert it to metric space!
+            metric_depth_base_log_space = torch.log(metric_depth_base + 1)
+            depth_11hw_aligned, _, _ = align_depth_least_square(
+                gt_arr=metric_depth_base_log_space.detach().float().cpu().numpy(),
+                pred_arr=depth_11hw.detach().float().cpu().numpy(),
+                valid_mask_arr=torch.ones_like(depth_11hw).bool().cpu().numpy(),
+                return_scale_shift=True,
+                max_resolution=None,
+            )
+            depth_11hw_aligned = torch.from_numpy(depth_11hw_aligned).to(device)
+
+            return torch.exp(depth_11hw_aligned) - 1
+
+            # sanity check (passes!)
+            # Input to pixel perfect depth. shape: (804, 848, 3), std: 82.61926369403086, mean: 88.29934251697487, dtype: uint8
+            # Output from pixel perfect depth. shape: (1, 1, 3, 804), std: 0.3026374280452728, mean: 0.4216078519821167, dtype: float32
+
+            # print(f"Input to pixel perfect depth. shape: {raw_image_hwc.shape}, std: {raw_image_hwc.std()}, mean: {raw_image_hwc.mean()}, dtype: {raw_image_hwc.dtype}")
+            # print(f"Resized output from pixel perfect depth. shape: {depth_11hw.shape}, std: {depth_11hw.std()}, mean: {depth_11hw.mean()}, dtype: {depth_11hw.dtype}")
+            # raise NotImplementedError("Pixel Perfect Depth is not implemented yet")
+
+    
+    else:
+        raise ValueError(f"Invalid base model: {base_model}")
+    
+    return base_depth_estimator_fn
---
+++ ppd_sharpdepth/infer_all.py
@@ -0,0 +1,122 @@
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
+import argparse
+import logging
+import os
+from enum import Enum
+
+os.environ["XFORMERS_DISABLED"] = "1"
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from omegaconf import OmegaConf
+from PIL import Image
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+from .sharpdepth.pipeline.pipeline import SharpDepthPipeline
+
+import debugpy
+
+from .base_depth_estimators import get_base_depth_estimator_fn
+from .depth_estimators import get_depth_estimator_fn, Model
+from .sharpdepth_kinds import SharpDepthKind
+
+
+if "__main__" == __name__:
+    logging.basicConfig(level=logging.INFO)
+
+    # -------------------- Arguments --------------------
+    parser = argparse.ArgumentParser(
+        description="Run single-image depth estimation using a specified model."
+    )
+    parser.add_argument(
+        "--checkpoint",
+        type=str,
+        default="prs-eth/marigold-v1-0",
+        help="Checkpoint path or hub name.",
+    )
+    parser.add_argument("--input_dir", type=str, required=True, help="Input image directory")
+    parser.add_argument("--output_dir", type=str, required=True, help="Output directory.")
+
+    # inference setting
+    parser.add_argument(
+        "--half_precision",
+        "--fp16",
+        action="store_true",
+        help="Run with half-precision (16-bit float), might lead to suboptimal result.",
+    )
+
+    parser.add_argument("--seed", type=int, default=None, help="Random seed.")
+
+    #parser.add_argument("--base_model", type=str, default="unidepth", help="Base model to use for depth estimation. Options: unidepth, depth_anything_small, depth_anything_large, pixel_perfect_depth")
+    parser.add_argument("--model", type=Model, default=Model.sharpdepth_unidepth, help="Model to use for depth estimation")
+    parser.add_argument("--debug", action="store_true", help="Debug mode.")
+
+    args = parser.parse_args()
+
+    if args.debug:
+        debugpy.listen(5678)
+        print("Waiting for debugger to attach...")
+        debugpy.wait_for_client()
+        print("Debugger attached")
+
+
+    checkpoint_path = args.checkpoint
+    output_dir = args.output_dir
+    input_dir = args.input_dir
+    half_precision = args.half_precision
+    model = args.model
+
+    seed = args.seed
+
+    print(f"arguments: {args}")
+
+    # -------------------- Preparation --------------------
+    # Print out config
+    logging.info(f"Inference settings: checkpoint = `{checkpoint_path}`, ")
+
+    os.makedirs(output_dir, exist_ok=True)
+    logging.info(f"output dir = {output_dir}")
+
+    # -------------------- Device --------------------
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+        logging.warning("CUDA is not available. Running on CPU will be slow.")
+    logging.info(f"device = {device}")
+
+    # -------------------- Model --------------------
+    if half_precision:
+        dtype = torch.float16
+        variant = "fp16"
+        logging.warning(f"Running with half precision ({dtype}), might lead to suboptimal result.")
+    else:
+        dtype = torch.float32
+        variant = None
+    
+    depth_estimator_fn = get_depth_estimator_fn(args.model, device, dtype)
+
+    #pipeline = SharpDepthPipeline.from_pretrained(checkpoint_path, sharpdepth_kind=SharpDepthKind.LOTUS, default_processing_resolution=768, default_denoising_steps=1)
+    #assert pipeline.default_processing_resolution == 768, f"default_processing_resolution = {pipeline.default_processing_resolution}, expected 768"
+    #assert pipeline.default_denoising_steps == 1, f"default_denoising_steps = {pipeline.default_denoising_steps}, expected 1"
+
+    #pipeline = pipeline.to(device, dtype=dtype)
+
+    #base_depth_estimator_fn = get_base_depth_estimator_fn(args.base_model, device, dtype)
+
+    imgs = sorted(os.listdir(input_dir))
+    # -------------------- Inference and saving --------------------
+    with torch.no_grad():
+        for batch in tqdm(imgs):
+            # Read input image
+            rgb = Image.open(os.path.join(input_dir, batch))
+            if args.debug: print("filename: ", os.path.join(input_dir, batch))
+            out = depth_estimator_fn(rgb)
+            #out = pipeline(rgb, base_depth_estimator_fn)
+
+            out.depth_base_colored.save(os.path.join(output_dir, batch.split(".")[0] + f"_{args.base_model}.jpg"))
+            out.depth_colored.save(os.path.join(output_dir, batch.split(".")[0] + f"_{args.base_model}_sharpdepth.png"))
+
