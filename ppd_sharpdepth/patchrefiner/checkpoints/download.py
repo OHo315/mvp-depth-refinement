7b3c86d4600a05e81523b7961198e26b73409a57
Owen Ho <oh315@cam.ac.uk>
2025-12-04 22:28:30 +0000

    Change zip url from public drive to my own drive.

--- ppd_sharpdepth/patchrefiner/checkpoints/download.py
+++ ppd_sharpdepth/patchrefiner/checkpoints/download.py
@@ -10,7 +10,7 @@ def download():
     Returns:
         None
     """
-    file_id = "1ifvbzCs9nAYFB9z3dAMgOZROo1tbWkc7"
+    file_id = "1X_XZVzo55imXzjkZSa5dHDrqDIHUZlc1"
     output_dir = "./ppd_sharpdepth/patchrefiner/checkpoints"
 
     # Ensure the directory exists
@@ -18,17 +18,35 @@ def download():
 
     zip_path = os.path.join(output_dir, "download.zip")
 
-    print(f"Downloading zip file...")
-    gdown.download(f"https://drive.google.com/uc?id={file_id}", zip_path, quiet=False)
-    
-    print("Downloaded zip file!")
+    if os.path.exists(zip_path):
+        if not zipfile.is_zipfile(zip_path):
+            print(f"File exists but not valid zip file. Attempting redownload...")
+            gdown.download(f"https://drive.google.com/uc?id={file_id}", zip_path, quiet=False, resume=True)
+            print("Download complete!")
+        else:
+            print(f"Zip file exists already. Skipping download.")
+    else:
+        print(f"Downloading zip file...")
+        gdown.download(f"https://drive.google.com/uc?id={file_id}", zip_path, quiet=False, resume=True)
+        print("Download complete!")
+
+    with open(zip_path, "rb") as f:
+        header = f.read(4)
+        print(header)
 
     print("Extracting ZIP...")
+    if not zipfile.is_zipfile(zip_path):
+        print("The provided zip file is not valid.\nPlease delete the zip file and restart the process.")
+        return
+
     with zipfile.ZipFile(zip_path, "r") as zip_ref:
         zip_ref.extractall(output_dir)
     
     print(f"Extracted zip file to {output_dir}")
 
-    # Delete ZIP file
-    os.remove(zip_path)
-    print("ZIP file deleted.")
+    ## Delete ZIP file
+    #os.remove(zip_path)
+    #print("ZIP file deleted.")
+
+if __name__ == "__main__":
+    download()
