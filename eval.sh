ecf3181136e553ec8c3895fc7de958e161eebf65
anthonyceponis <anthonyceponis@gmail.com>
2025-12-04 20:29:24 +0000

    Implement depth visualisation and refactor infer script to use config files.

--- eval.sh
+++ eval.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+#python -m ppd_sharpdepth.eval \
+#	--dataset_config_path config/dataset_depth/data_middlebury_test.yaml \
+#	--model_architecture pixelperfectdepth
+
 python -m ppd_sharpdepth.eval \
 	--dataset_config_path config/dataset_depth/data_nyu_test.yaml \
-	--model_architecture unidepth
+	--model_architecture pixelperfectdepth
 
