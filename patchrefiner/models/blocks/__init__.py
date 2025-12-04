2082bac15985ce419bb8035f48897e832f015dec
Owen Ho <oh315@cam.ac.uk>
2025-12-04 20:56:36 +0000

    Remove previous patchrefiner implementation

--- patchrefiner/models/blocks/__init__.py
+++
@@ -1,3 +0,0 @@
-from .guided_fusion_model import GuidedFusionPatchFusion
-from .swin_layers import G2LFusion
-from .fusion_model import FusionUnet
--- patchrefiner/models/builder.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-import warnings
-
-from registry import MODELS
-
-def build_model(cfg):
-    """Build backbone."""
-    return MODELS.build(cfg)
