0bbcb673a02ac351e989d998a6e3e4551431b197
Owen Ho <oh315@cam.ac.uk>
2025-12-06 01:56:56 +0000

    Hopefully fix module import errors

--- ppd_sharpdepth/patchrefiner/estimator/registry/registry.py
+++ ppd_sharpdepth/patchrefiner/estimator/registry/registry.py
@@ -4,5 +4,5 @@ from mmengine import Registry
 from mmengine.registry import MODELS as MMENGINE_MODELS
 from mmengine.registry import DATASETS as MMENGINE_DATASETS
 
-MODELS = Registry('model', parent=MMENGINE_MODELS, locations=['estimator.models'])
-DATASETS = Registry('dataset', parent=MMENGINE_DATASETS, locations=['estimator.datasets'])
+MODELS = Registry('model', parent=MMENGINE_MODELS, locations=['ppd_sharpdepth.patchrefiner.estimator.models'])
+DATASETS = Registry('dataset', parent=MMENGINE_DATASETS, locations=['ppd_sharpdepth.patchrefiner.estimator.datasets'])
