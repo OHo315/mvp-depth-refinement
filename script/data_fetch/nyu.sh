3a835d2e56d0ce66130c0e2361f35d08ccd0dba5
Andrew Healey <doolie.healey@gmail.com>
2025-12-08 18:04:46 +0000

    use faster hf download

--- script/data_fetch/nyu.sh
+++ script/data_fetch/nyu.sh
@@ -1,6 +1,7 @@
 echo "Fetching NYU V2 dataset..."
 mkdir $BASE_DATA_DIR/nyu_v2_raw
-wget -O $BASE_DATA_DIR/nyu_v2_raw/nyu_depth_v2_labeled.mat "http://horatio.cs.nyu.edu/mit/silberman/nyu_depth_v2/nyu_depth_v2_labeled.mat"
+# wget -O $BASE_DATA_DIR/nyu_v2_raw/nyu_depth_v2_labeled.mat "http://horatio.cs.nyu.edu/mit/silberman/nyu_depth_v2/nyu_depth_v2_labeled.mat"
+hf download "andrew-healey/nyuv2" nyu_depth_v2_labeled.mat $BASE_DATA_DIR/nyu_v2_raw/nyu_depth_v2_labeled.mat
 
 echo "Preprocessing NYU V2 dataset..."
 python3 script/depth/dataset_preprocess/nyu/nyu_preprocess.py
