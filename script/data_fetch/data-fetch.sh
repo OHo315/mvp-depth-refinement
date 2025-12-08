0a9f7762a042a337b0e643405d1c3b72d2c7b4ab
Andrew Healey <doolie.healey@gmail.com>
2025-12-08 18:12:35 +0000

    parallelize data-fetch

--- script/data_fetch/data-fetch.sh
+++ script/data_fetch/data-fetch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 pip install unzip
 
-bash ./script/data_fetch/hypersim-hf.sh
-bash ./script/data_fetch/nyu.sh
-bash ./script/data_fetch/middlebury.sh
-bash ./script/data_fetch/arkitscenes-hf.sh
-bash ./script/data_fetch/waymo.sh
-bash ./script/data_fetch/diode-indoor.sh
-
+bash ./script/data_fetch/hypersim-hf.sh &
+bash ./script/data_fetch/nyu.sh &
+bash ./script/data_fetch/middlebury.sh &
+bash ./script/data_fetch/arkitscenes-hf.sh &
+bash ./script/data_fetch/waymo.sh & 
+bash ./script/data_fetch/diode-indoor.sh &
+echo "Waiting for all datasets to be fetched..." &
+wait
