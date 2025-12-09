296dc77b4acb6fa4ef8542bbc0452677528ebd47
Andrew Healey <doolie.healey@gmail.com>
2025-12-09 12:05:01 +0000

    change default lotus config

--- ppd_sharpdepth/training/train_lotus.sh
+++ ppd_sharpdepth/training/train_lotus.sh
@@ -6,7 +6,7 @@ echo $WORKSPACE_DIR
 echo $PYTHONPATH
 
 num_gpus=$NUM_GPUS
-macrobatch_size=1
+macrobatch_size=8
 
 gradient_accumulation_steps=$((macrobatch_size / num_gpus))
 
@@ -20,17 +20,17 @@ accelerate launch --num_processes $num_gpus ppd_sharpdepth/training/train.py \
     --mixed_precision bf16 \
     --seed 42 \
     --allow_tf32 \
-    --learning_rate 1e-5 \
+    --learning_rate 5e-5 \
     --lr_scheduler cosine \
     --lr_warmup_steps 100 \
     --tracker_project_name lotus_train_andrew \
-    --wandb_name "lotus" \
+    --wandb_name "lotus_lr_5e-5" \
     --set_grads_to_none \
     --checkpointing_steps 500 \
     --validation_steps 100 \
     --train_batch_size 1 \
     --gradient_accumulation_steps $gradient_accumulation_steps \
-    --num_train_epochs 1 \
+    --num_train_epochs 8 \
     --use_ema \
     --base_data_dir "$WORKSPACE_DIR/data/" \
     --config "$WORKSPACE_DIR/config/train_marigold_depth.yaml" \
@@ -38,4 +38,5 @@ accelerate launch --num_processes $num_gpus ppd_sharpdepth/training/train.py \
     --base_model unidepth \
     --denoiser lotus \
     --use_conditioning_probability 0.8 \
+    --align_depth_least_square \
     "$@"
